0f240e301d97944841c0f2ad5a4a3815125337aa
yoonseo <cysyes0403@naver.com>
2026-02-11 19:17:15 +0900

    Update: 스테이지 문 로직

--- Source/ProjectSanzo/Private/Stage/SanzoStageGate.cpp
+++ Source/ProjectSanzo/Private/Stage/SanzoStageGate.cpp
@@ -1,23 +1,53 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Stage/SanzoStageGate.h"
 
 ASanzoStageGate::ASanzoStageGate()
 {
 	PrimaryActorTick.bCanEverTick = false;
+  Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+  SetRootComponent(Root);
 
+  GateMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GateMesh"));
+  GateMesh->SetupAttachment(Root);
+
+  TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+  TriggerBox->SetupAttachment(Root);
+
+  TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+  TriggerBox->SetCollisionResponseToAllChannels(ECR_Ignore);
+  TriggerBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void ASanzoStageGate::OpenGate()
+{
+  if (bIsOpened) return;
+  UE_LOG(LogTemp, Warning, TEXT("SG: 문 열림"));
+  bIsOpened = true;
 }
 
 void ASanzoStageGate::BeginPlay()
 {
-	Super::BeginPlay();
-	
+  Super::BeginPlay();
+  TriggerBox->OnComponentBeginOverlap.AddDynamic(
+    this,
+    &ASanzoStageGate::OnOverlapBegin
+  );
 }
 
-void ASanzoStageGate::Tick(float DeltaTime)
+void ASanzoStageGate::OnOverlapBegin(
+  UPrimitiveComponent* OverlappedComponent,
+  AActor* OtherActor,
+  UPrimitiveComponent* OtherComp,
+  int32 OtherBodyIndex,
+  bool bFromSweep,
+  const FHitResult& SweepResult)
 {
-	Super::Tick(DeltaTime);
+  if (!bIsOpened) return;
 
+  if (OtherActor && OtherActor->ActorHasTag("Player"))
+  {
+    UE_LOG(LogTemp, Warning, TEXT("SG: 플레이어 들어감"));
+
+    // 여기서 StageManager에 다음 방 이동 요청
+    OnGateEntered.Broadcast();
+  }
 }
-
