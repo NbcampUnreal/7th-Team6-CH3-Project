fb3ef99e11c0455c8ade04acfef76d8e143ea49c
djkim12 <agwx1223@naver.com>
2026-02-11 12:02:42 +0900

    Add: 적 AI 이동, 플레이어 감지, 추격
    C++ Class
    - SanzoBTService_Detect
    - SanzoBTTask_Patrol

---
+++ Source/ProjectSanzo/Private/AI/BT/SanzoBTService_Detect.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AI/BT/SanzoBTService_Detect.h"
+#include "AIController.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+USanzoBTService_Detect::USanzoBTService_Detect()
+{
+  NodeName = TEXT("Detect & Speed Control");
+  Interval = 0.5f; // 0.5초마다 실행 (최적화)
+}
+
+void USanzoBTService_Detect::TickNode(
+  UBehaviorTreeComponent& OwnerComp,
+  uint8* NodeMemory,
+  float DeltaSeconds)
+{
+  Super::TickNode(
+    OwnerComp,
+    NodeMemory,
+    DeltaSeconds);
+
+  APawn* ControllingPawn = OwnerComp.GetAIOwner()->GetPawn();
+  if (!ControllingPawn) return;
+
+  ACharacter* AIChar = Cast<ACharacter>(ControllingPawn);
+  if (!AIChar) return;
+
+  UObject* TargetObject = OwnerComp.GetBlackboardComponent()
+    ->GetValueAsObject(TEXT("TargetActor"));
+
+  if (TargetObject)
+  {
+    // 타겟 발견 시 -> 달리기
+    AIChar->GetCharacterMovement()->MaxWalkSpeed = ChaseSpeed;
+  }
+  else
+  {
+    // 타겟 없음 -> 걷기
+    AIChar->GetCharacterMovement()->MaxWalkSpeed = PatrolSpeed;
+  }
+}
+
---
+++ Source/ProjectSanzo/Private/AI/BT/SanzoBTTask_Patrol.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BT/SanzoBTTask_Patrol.h"
+#include "AIController.h"
+#include "NavigationSystem.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+USanzoBTTask_Patrol::USanzoBTTask_Patrol()
+{
+  NodeName = TEXT("Find Patrol Pos");
+}
+
+EBTNodeResult::Type USanzoBTTask_Patrol::ExecuteTask
+(
+  UBehaviorTreeComponent& OwnerComp,
+  uint8* NodeMemory
+)
+{
+  AAIController* AIController = OwnerComp.GetAIOwner();
+  APawn* ControllingPawn = AIController ? AIController->GetPawn() : nullptr;
+
+  if (ControllingPawn)
+  {
+    UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetCurrent(GetWorld());
+    FNavLocation RandomLocation;
+
+    // 내 위치 기준 반경 내 랜덤 위치 찾기
+    if (NavSystem &&
+      NavSystem->GetRandomPointInNavigableRadius(ControllingPawn
+        ->GetActorLocation(),
+        SearchRadius,
+        RandomLocation))
+    {
+      // 찾은 위치를 블랙보드(TargetLocation)에 저장
+      OwnerComp.GetBlackboardComponent()
+        ->SetValueAsVector(GetSelectedBlackboardKey(),
+          RandomLocation.Location);
+      return EBTNodeResult::Succeeded;
+    }
+  }
+  return EBTNodeResult::Failed;
+}
+
