5d1c7d51220fad00f7c369873e1cee0f053078e8
JRoLee <cnsfw123@gmail.com>
2026-02-11 17:15:02 +0900

    Add : SanzoMainWidget, SanzoPopUpWidget
    Add
    SanzoMainWidget class,
    SanzoPopUpWidget class

---
+++ Source/ProjectSanzo/Public/UI/SanzoMainWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SanzoMainWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSANZO_API USanzoMainWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
