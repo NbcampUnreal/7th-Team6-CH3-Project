1bdc4a845aa66710fab08022a0c9b10abf486c31
JRoLee <cnsfw123@gmail.com>
2026-02-11 16:56:25 +0900

    Refactor : HUD Code refactoring
    make base code before merge

--- Source/ProjectSanzo/Public/UI/SanzoHUDWidget.h
+++ Source/ProjectSanzo/Public/UI/SanzoHUDWidget.h
@@ -9,6 +9,44 @@
 UCLASS()
 class PROJECTSANZO_API USanzoHUDWidget : public UUserWidget
 {
-  GENERATED_BODY()
+	GENERATED_BODY()
+#pragma region UI
 
+public:
+	//초기 생성 로직
+	virtual void NativeConstruct() override;
+
+	//Stat Component 로 부터 받아올 정보 위젯 연결
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* ExpBar;
+
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* HealthBar;
+
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* StaminaBar;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* LevelText;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* AmmoCountText;
+
+	UPROPERTY(meta = (BindWidget))
+	class UImage* WeaponImage;
+	//추후 함수 수정 필요
+	void UpdateHUD();
+
+	//Game State로 부터 받아올 정보 연결
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* StageText;
+
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* StageProgressBar;
+
+	UFUNCTION()
+	void UpdateStageProgressBar();
+
+
+#pragma endregion 이준로
 };
