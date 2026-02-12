a519a1c561aa93fa3d8e58687cd51c7f2d501b22
JRoLee <cnsfw123@gmail.com>
2026-02-12 14:51:00 +0900

    Refactor : HUD class, Stat Component, Game State
    Refactor HUD class, Stat Component, Game State

--- Source/ProjectSanzo/Private/Character/Components/SanzoStatComponent.cpp
+++ Source/ProjectSanzo/Private/Character/Components/SanzoStatComponent.cpp
@@ -5,47 +5,75 @@
 
 USanzoStatComponent::USanzoStatComponent()
 {
-  CurrentStamina = 100.f;
-  MaxStamina = 100.f;
-  StaminaRegenRate = 5.f;
-  StaminaConsumptionRate = 10.f;
-  StaminaThresholdForActions = 20.f;
-  StaminaRegenDelay = 2.f;
-  TimeSinceLastStaminaUse = 0.f;
-  DodgeStaminaCost = 15.f;
-  SprintStaminaCostPerSecond = 5.f; 
+	CurrentStamina = 100.f;
+	MaxStamina = 100.f;
+	StaminaRegenRate = 5.f;
+	StaminaConsumptionRate = 10.f;
+	StaminaThresholdForActions = 20.f;
+	StaminaRegenDelay = 2.f;
+	TimeSinceLastStaminaUse = 0.f;
+	DodgeStaminaCost = 15.f;
+	SprintStaminaCostPerSecond = 5.f;
 
-  PrimaryComponentTick.bCanEverTick = false;
+	//테스트 코드
+	CurrentHealth = 50.f;
+	MaxHealth = 100.f;
 
-  // ...
+	CurrentExp = 20.f;
+	MaxExp = 100.f;
+	Level = 32;
+
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
 }
 
 
 void USanzoStatComponent::BeginPlay()
 {
-  Super::BeginPlay();
+	Super::BeginPlay();
 
-  // ...
-
+	// ...
 }
 
 
-void USanzoStatComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void USanzoStatComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                        FActorComponentTickFunction* ThisTickFunction)
 {
-  Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-  // ...
+	// ...
 }
 
 void USanzoStatComponent::ConsumeStamina(float Amount)
 {
-  
-  CurrentStamina -= Amount;
-  
-  
-  if(Amount <= 0.f)
-  {
-    return;
-  } 
+	CurrentStamina -= Amount;
+
+
+	if (Amount <= 0.f)
+	{
+	}
+}
+#pragma region UIUpdateBroadCastStat
+// 스탯 변경 방송 함수
+// 스탯 변경되는 모든 함수 끝에 붙여주셔야 합니다!
+void USanzoStatComponent::BroadCastStatUpdate()
+{
+	if (OnStatChanged.IsBound())
+	{
+		OnStatChanged.Broadcast(MakeStatData());
+	}
 }
 
+// 변경 스탯 구조체 저장 함수
+FSanzoStatData USanzoStatComponent::MakeStatData() const
+{
+	FSanzoStatData Data;
+	Data.HealthPercent = (MaxHealth > 0.f) ? CurrentHealth / MaxHealth : 0.f;
+	Data.StaminaPercent = (MaxStamina > 0.f) ? CurrentStamina / MaxStamina : 0.0f;
+	Data.ExpPercent = (MaxExp > 0.f) ? CurrentExp / MaxExp : 0.f;
+	Data.CurrentLevel = Level;
+
+	return Data;
+}
+#pragma endregion 이준로
--- Source/ProjectSanzo/Private/UI/SanzoHUDWidget.cpp
+++ Source/ProjectSanzo/Private/UI/SanzoHUDWidget.cpp
@@ -3,7 +3,47 @@
 
 #include "UI/SanzoHUDWidget.h"
 
+#include "Character/Components/SanzoStatComponent.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
 void USanzoHUDWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
+
+	if (APawn* PlayerCharacter = GetOwningPlayerPawn())
+	{
+		//StatComponent 추출
+		USanzoStatComponent* StatComponent = PlayerCharacter->FindComponentByClass<USanzoStatComponent>();
+		if (StatComponent)
+		{
+			//Delegate 등록
+			//this : 대상 객체, &USanzoHUDWidget::HandleStatChanged: 실행할 함수
+			StatComponent->OnStatChanged.AddDynamic(this, &USanzoHUDWidget::HandleStatChanged);
+		}
+	}
+}
+
+void USanzoHUDWidget::HandleStatChanged(const FSanzoStatData& Data)
+{
+	if (HealthBar)
+	{
+		HealthBar->SetPercent(Data.HealthPercent);
+	}
+	if (StaminaBar)
+	{
+		StaminaBar->SetPercent(Data.StaminaPercent);
+	}
+	if (ExpBar)
+	{
+		ExpBar->SetPercent(Data.ExpPercent);
+	}
+	if (LevelText)
+	{
+		LevelText->SetText(FText::AsNumber(Data.CurrentLevel));
+	}
+}
+
+void USanzoHUDWidget::UpdateStageProgressBar()
+{
 }
