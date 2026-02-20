0e58735428eb119eafd4a6d9d1ff83a7c48261bd
kbrother102 <kbrother102@gmail.com>
2026-02-20 18:42:52 +0900

    Update 캐릭터 태그 3차 개편

--- Source/ProjectSanzo/Private/Common/SanzoGameplayTag.cpp
+++ Source/ProjectSanzo/Private/Common/SanzoGameplayTag.cpp
@@ -1,7 +1,17 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Common/SanzoGameplayTag.h"
+
+UE_DEFINE_GAMEPLAY_TAG(SanzoTags::Aiming, "Character.Action.Movable.Aiming");
+UE_DEFINE_GAMEPLAY_TAG(SanzoTags::Sprint, "Character.Action.Movable.Sprint");
+UE_DEFINE_GAMEPLAY_TAG(SanzoTags::Attack, "Character.Action.Movable.Attack");
+UE_DEFINE_GAMEPLAY_TAG(SanzoTags::Parry, "Character.Action.Fixed.Parry");
+UE_DEFINE_GAMEPLAY_TAG(SanzoTags::Dodge, "Character.Action.Fixed.Dodge");
+UE_DEFINE_GAMEPLAY_TAG(SanzoTags::Exhausted, "Character.Status.Exhausted");
+
+UE_DEFINE_GAMEPLAY_TAG(SanzoTags::Action_Fixed, "Character.Action.Fixed");
+
 
 SanzoGameplayTag::SanzoGameplayTag()
 {
--- Source/ProjectSanzo/Public/Common/SanzoGameplayTag.h
+++ Source/ProjectSanzo/Public/Common/SanzoGameplayTag.h
@@ -1,12 +1,28 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
-
+#include "GameplayTagContainer.h"
+#include "NativeGameplayTags.h"
 /**
  * 
  */
+namespace SanzoTags
+{
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Aiming);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sprint);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attack);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Exhausted);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Parry);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Dodge);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Action_Fixed);
+}
+//필요한 태그를 미리 선언	
+//사용법 SanzoTags::Aiming, SanzoTags::Sprint, SanzoTags::Attack, SanzoTags::Exhausted
+
+
+
 class PROJECTSANZO_API SanzoGameplayTag
 {
 public:
