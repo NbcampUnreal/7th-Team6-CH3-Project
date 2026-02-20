2cc03c9ac2d1f5ddfbe944c562eeb345888322c6
kbrother102 <kbrother102@gmail.com>
2026-02-20 14:48:08 +0900

    Add 태그를 모아두는 클래스 생성

---
+++ Source/ProjectSanzo/Private/Common/SanzoGameplayTag.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Common/SanzoGameplayTag.h"
+
+SanzoGameplayTag::SanzoGameplayTag()
+{
+}
+
+SanzoGameplayTag::~SanzoGameplayTag()
+{
+}
---
+++ Source/ProjectSanzo/Public/Common/SanzoGameplayTag.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROJECTSANZO_API SanzoGameplayTag
+{
+public:
+	SanzoGameplayTag();
+	~SanzoGameplayTag();
+};
