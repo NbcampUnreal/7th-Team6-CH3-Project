81668d008f9e4cf88779ee26f7105270d62a4252
kbrother102 <kbrother102@gmail.com>
2026-02-20 10:48:14 +0900

    Add 회피 NotifyState, Parry NotifyState 추가

---
+++ Source/ProjectSanzo/Public/Notifies/SanzoANS_ParryResponse.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "SanzoANS_ParryResponse.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSANZO_API USanzoANS_ParryResponse : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
