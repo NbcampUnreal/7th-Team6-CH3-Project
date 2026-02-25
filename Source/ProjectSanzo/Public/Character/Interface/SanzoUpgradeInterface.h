a2ea203a6271e71323b0afddbbcafeb8b6a18daf
kbrother102 <kbrother102@gmail.com>
2026-02-25 14:45:20 +0900

    Add 업그레이드를 위한 인터페이스 추가

---
+++ Source/ProjectSanzo/Public/Character/Interface/SanzoUpgradeInterface.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "SanzoUpgradeInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class USanzoUpgradeInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PROJECTSANZO_API ISanzoUpgradeInterface
+{
+	GENERATED_BODY()
+	virtual void ApplyUpgrade(struct EUpgradeTarget Target, struct EUpgradeType Type, float Value) = 0;
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
