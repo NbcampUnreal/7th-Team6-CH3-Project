99c350ff8705b66a2b843706302e3bfe688f82b2
yoonseo <cysyes0403@naver.com>
2026-02-26 11:24:43 +0900

    Update: Hit, Death, Stun Sound

--- Source/ProjectSanzo/Public/AI/SanzoEnemyBase.h
+++ Source/ProjectSanzo/Public/AI/SanzoEnemyBase.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
@@ -196,5 +196,14 @@ protected:
   UFUNCTION()
   virtual void OnParriedCallback();
 #pragma endregion 김동주
+
+#pragma region Sound
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sounds")
+  TArray<USoundBase*> DeathSounds;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sounds")
+  TArray<USoundBase*> HitSounds;
+  UPROPERTY(EditAnywhere, Category = "Sounds")
+  USoundAttenuation* EnemyAttenuation;
+#pragma endregion 최윤서
 
 };
