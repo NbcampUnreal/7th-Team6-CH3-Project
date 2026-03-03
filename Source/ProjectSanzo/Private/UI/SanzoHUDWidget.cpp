07f0c9459da9ecd8d819397a31a4e8dc8a63d8cf
JRoLee <cnsfw123@gmail.com>
2026-03-03 12:02:50 +0900

    Update : change display stamina to int
    Update : change display stamina to int

--- Source/ProjectSanzo/Private/UI/SanzoHUDWidget.cpp
+++ Source/ProjectSanzo/Private/UI/SanzoHUDWidget.cpp
@@ -113,9 +113,10 @@ void USanzoHUDWidget::HandleStaminaBarData(float MaxStamina, float CurrentStamin
 {
 	if (StaminaInfoText)
 	{
+	  int32 DisplayCurrentStamina = FMath::FloorToInt(CurrentStamina);
 		StaminaInfoText->SetText(FText::Format(
 			FText::FromString(TEXT("{0} / {1}"))
-			, FText::AsNumber(CurrentStamina)
+			, FText::AsNumber(DisplayCurrentStamina)
 			, FText::AsNumber(MaxStamina)));
 	}
 
